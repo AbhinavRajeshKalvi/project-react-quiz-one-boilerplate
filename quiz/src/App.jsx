b7e61da46c332e00210a95b22f0f9d9402e0ca4c
AbhinavRajeshKalvi <abhinav.rajesh@kalvium.community>
2023-12-16 10:13:25 +0530

    0

---
+++ quiz/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import HomeComp from './components/HomeComponent'
+import QuizComp from './components/QuizComponent';
+import ResultComp from './components/ResultComponent';
+
+
+function App() {
+  return (
+    <div className="App">
+    <HomeComp />
+    </div>
+  );
+}
+
+export default App;
